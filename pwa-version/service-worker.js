ce5196e69308c2978c38526852c5480a53c9ac28
gbaney-uk <gbaney@googlemail.com>
2026-06-28 08:42:16 +0100

    Create service-worker.js
    Updated - removed buttons print and save

---
+++ pwa-version/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "bowls-rink-picker-v7";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return (
+        cachedResponse ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
+});
